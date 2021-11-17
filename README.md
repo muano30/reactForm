9a34c497e1f6a1bb42dcb4d5c0d7aebcffcd5909
muano30 <muanonevhufumba@gmail.com>
2021-11-17 09:27:59 +0200

    adding readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+
+# This is what this form does
+
+When a user submit the name the output should be the name they have submitted.
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +74,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
--- src/App.js
+++ src/App.js
@@ -1,43 +1,46 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import './App.css'
 
 
-function Form(){
-const [formInfo, setFormInfo] = useState({
-    name: "",
-});
+function Form() {
+    const [formInfo, setFormInfo] = useState({
+        name: "",
+    });
 
-const [list, setList] = useState([]);
+    const [list, setList] = useState([]);
 
-const handleChange = (e) => {
-    setFormInfo({...formInfo, [e.target.name]: e.target.value})
-};
+    const handleChange = (e) => {
+        setFormInfo({ ...formInfo, [e.target.name]: e.target.value })
+    };
 
-const handleSubmit = (e) => {
-    e.preventDefault();
-    setList([...list, formInfo])
-    setFormInfo({name:''})
-};
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setList([...list, formInfo])
+        setFormInfo({ name: '' })
+    };
 
-    return(
-        <div>
+    return (
+        <div className="form">
+            <h1>SUBMIT YOUR NAME</h1>
+
             <form onSubmit={handleSubmit}>
                 <label>Name</label>
                 <input
-                type="text"
-                onChange={handleChange}
-                name = "name" 
-                value={formInfo.name}
+                    type="text"
+                    onChange={handleChange}
+                    name="name"
+                    value={formInfo.name}
                 />
-                <button type="submit">Submit</button>  
+                <button type="submit">Submit</button>
             </form>
 
             <ul>
                 {list.map((item) => {
-                    return(
-                    <li>
-                        <h2>{item.name}</h2>
-                    </li>)
-                })}     
+                    return (
+                        <li>
+                            <h2>{item.name}</h2>
+                        </li>)
+                })}
             </ul>
 
         </div>
